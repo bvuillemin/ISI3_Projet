0a11a4b3413630cc60fb1bc0e0600efc71c95e81
Tom <thomas.coquan@etu.univ-lyon1.fr>
2015-05-06 17:59:12 +0200

    Run et Test Robot

--- src/main/java/Modele/Robot.java
+++ src/main/java/Modele/Robot.java
@@ -7,11 +7,12 @@ import java.util.Map;
 /**
  * Created by thomascoquan on 06/05/2015.
  */
-public class Robot {
+public class Robot extends Thread{
 
     protected Noeud noeudActuel;
     protected ArrayList<typeArc> listTypeArcTraversable;
     protected ArrayList<Arc> listArcPlusCourt;
+    protected ArrayList<Arc> chemin;
     protected Graphe graphe;
     protected Map dijkstra;
 
@@ -28,12 +29,20 @@ public class Robot {
         return listTypeArcTraversable;
     }
 
+    public ArrayList<Arc> getChemin() {
+        return chemin;
+    }
+
     public void setNoeudActuel(Noeud noeudActuel) {
         this.noeudActuel = noeudActuel;
     }
 
     public void setListTypeArcTraversable(ArrayList<typeArc> listTypeArcTraversable) {
         this.listTypeArcTraversable = listTypeArcTraversable;
+    }
+
+    public void setChemin(ArrayList<Arc> chemin) {
+        this.chemin = chemin;
     }
 
     public ArrayList<Arc> plusCourtChemin(Noeud noeudDestination){
@@ -46,7 +55,18 @@ public class Robot {
         return listArcPlusCourt;
     }
 
-    public void aller (Noeud noeudDestination) {
-
+    @Override
+    public void run() {
+        Arc morceauChemin;
+        while(chemin.isEmpty()==false) {
+            try {
+                morceauChemin=chemin.get(0);
+                noeudActuel=morceauChemin.getNoeud2();
+                chemin.remove(morceauChemin);
+                wait(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
